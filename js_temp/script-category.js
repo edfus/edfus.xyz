db42fef3c9cc9852c0aa23d1655ee5e7a4e1842b
edfus <edfusedfus@gmail.com>
2020-07-16 22:34:35 +0800

    Site updated: 2020-07-16 22:34:24

--- js_temp/script-category.js
+++ js_temp/script-category.js
@@ -1,1 +1,214 @@
-"use strict";!function(){var e=!1;document.getElementById("nav-search-btn").addEventListener("click",function(){e||(e=!0,document.getElementById("search-form-wrap").classList.add("on"),setTimeout(function(){e=!1,document.getElementById("search-input").focus()},200))},!1),document.getElementById("nav-search-btn").addEventListener("focusin",function(){e||(e=!0,document.getElementById("search-form-wrap").classList.add("on"),setTimeout(function(){e=!1,document.getElementById("search-input").focus()},200))},!1),document.getElementById("search-input").addEventListener("focusout",function(){document.getElementById("search-form-wrap").classList.remove("on"),setTimeout(function(){e=!1},200)},!1);var t=!1;document.getElementById("main-nav-toggle").addEventListener("click",function(){t||(t=!0,document.getElementById("container").classList.toggle("mobile-nav-on"),setTimeout(function(){t=!1},200))},!1),document.getElementById("wrap").addEventListener("click",function(){!t&&document.getElementById("container").classList.contains("mobile-nav-on")&&document.getElementById("container").classList.remove("mobile-nav-on")}),window.goToTop=function(){var e=document.querySelector("a.gotop"),t=null;window.addEventListener("scroll",function(){null===t&&(t=setTimeout(function(){300<window.scrollY?e.classList.add("show"):e.classList.remove("show"),t=null},200))},!1)},"loading"!==document.readyState?setTimeout(function(){return window.goToTop()},1):document.addEventListener("DOMContentLoaded",function(){return window.goToTop()});var i=document.querySelector("footer#footer");!function(e){function t(e){document.documentElement.scrollHeight-4<=document.documentElement.clientHeight?e.style.cssText="position:absolute;left:0;bottom:0":e.style.cssText=""}var n=0<arguments.length&&void 0!==e?e:i;t(n);var o="onorientationchange"in window?"orientationchange":"resize";window.addEventListener(o,function(){return t(n)},!1)}();function n(){s.style.display="none",Array.from(s.querySelectorAll(".dropdown-item")).forEach(function(e){e.classList.remove("show")})}var o=!1,c=!1,s=document.getElementById("dropdown-whole-control"),d=Array.from(document.querySelectorAll(".to-be-active")),a=document.getElementById("drop"),r=!1,u=s.querySelector(".current.dropdown-item");s.addEventListener("mouseenter",function(){o=!0},!1),s.addEventListener("mouseleave",function(){o=!1,setTimeout(function(){c||(a.classList.remove("active"),n())},200)},!1),d.forEach(function(e){e.addEventListener("mouseenter",function(){e.classList.add("active")},!1),e.addEventListener("mouseleave",function(){e.classList.remove("active")},!1)});var l=Array.from(s.querySelectorAll(".dropdown-item:not(.current)"));s.addEventListener("focusin",function(){o=!0},!1),l.forEach(function(e){e.addEventListener("focus",function(){o=!0,e.classList.add("active")},!1),e.addEventListener("blur",function(){e.classList.remove("active"),o=!1,setTimeout(function(){c||o||(a.classList.remove("active"),n())},1)},!1)});function m(){c=!0,s.style.display=f,u.classList.add("show"),l.forEach(function(e,t){setTimeout(function(){e.classList.add("show")},80*t)}),!1===r&&("flex"!=f?(s.style.top=a.offsetTop+12-u.offsetTop+"px",s.style.left=a.clientWidth-2+"px"):(s.style.top=a.offsetTop-u.offsetTop-5+"px",s.style.left=s.clientWidth-u.offsetLeft+"px",a.style.paddingRight=s.clientWidth-u.offsetLeft-54+"px"),r=!0)}var f=window.matchMedia("(max-width: 1330px)").matches?"flex":"block";a.addEventListener("mouseenter",function(){m()},!1),a.addEventListener("focus",function(){m()},!1);function v(e){c=!1,a.classList.add("active"),setTimeout(function(){o||(a.classList.remove("active"),n())},e)}a.addEventListener("mouseleave",function(){v(200)},!1),a.addEventListener("blur",function(){v(1)},!1)}();
+(()=>{
+    // Search
+  {
+    let isSearchBoxExpanded = false;
+
+    let stopSearchAnim = function(callback){
+      
+    };
+
+    document.getElementById('nav-search-btn').addEventListener('click', function(){
+      if (isSearchBoxExpanded) return;
+
+      isSearchBoxExpanded = true;
+      document.getElementById('search-form-wrap').classList.add('on');
+      setTimeout(()=>{
+        isSearchBoxExpanded = false;
+        document.getElementById('search-input').focus();
+      }, 200); 
+    },false);
+    document.getElementById('nav-search-btn').addEventListener('focusin',()=>{
+      if (isSearchBoxExpanded) return;
+
+      isSearchBoxExpanded = true;
+      document.getElementById('search-form-wrap').classList.add('on');
+      setTimeout(()=>{
+        isSearchBoxExpanded = false;
+        document.getElementById('search-input').focus();
+      }, 200);
+    },false)
+
+    document.getElementById('search-input').addEventListener('focusout',()=>{
+      document.getElementById('search-form-wrap').classList.remove('on');
+      setTimeout(()=>{
+        isSearchBoxExpanded = false;
+      }, 200);
+    },false);
+  }
+  // Mobile nav
+  {
+    let isMobileNavExpanded = false;
+
+    document.getElementById('main-nav-toggle').addEventListener('click', function(){
+      if (isMobileNavExpanded) return;
+      isMobileNavExpanded = true;
+      document.getElementById('container').classList.toggle('mobile-nav-on');
+      setTimeout(()=>{
+        isMobileNavExpanded = false;
+      }, 200);
+    },false);
+
+    document.getElementById('wrap').addEventListener('click', function(){
+      if (isMobileNavExpanded || !document.getElementById('container').classList.contains('mobile-nav-on')) return;
+      document.getElementById('container').classList.remove('mobile-nav-on');
+    });
+  }
+
+  window.goToTop = function() {
+    const topElement = document.querySelector('a.gotop');
+    const min = 300;
+    var timer = null;   
+    window.addEventListener("scroll",()=>{
+          if (timer === null) {               
+            timer = setTimeout(()=>{
+              let scrollTop = window.scrollY;
+              if (scrollTop > min) {
+                topElement.classList.add('show');
+              } else {
+                topElement.classList.remove('show');
+              }
+              timer = null;
+            }, 200);
+          }
+      },false);
+  };
+  if (document.readyState !== "loading") {
+    setTimeout(()=>window.goToTop(), 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", ()=>window.goToTop());
+  }
+  
+//footer fixed to bottom
+  const footer = document.querySelector("footer#footer");
+  ((e = footer) => {
+    
+    const fixFooter = (e) => {
+      if(document.documentElement.scrollHeight - 4 <= document.documentElement.clientHeight){
+        e.style.cssText = "position:absolute;left:0;bottom:0";
+      }else{
+        e.style.cssText = "";
+      }
+    }
+    fixFooter(e);
+    //orientationchange is deprecated. But I have no idea what else should I use.
+    const orientationEvent = "onorientationchange" in window ? "orientationchange" : "resize";
+    window.addEventListener(orientationEvent, () => fixFooter(e), false);
+  })()
+    
+  //category界面的hover
+  {
+    let mouseOnWhole = false, mouseOnToggle = false;
+    const wholeControl = document.getElementById('dropdown-whole-control');
+    const toBeActiveArray = Array.from(document.querySelectorAll('.to-be-active'));
+    const dropdownToggleEle = document.getElementById('drop');
+
+    let setStyleOfDiv = false;
+    const currentCategory =  wholeControl.querySelector('.current.dropdown-item');
+    
+    wholeControl.addEventListener("mouseenter", ()=>{
+        mouseOnWhole = true;
+    }, false);
+
+    wholeControl.addEventListener("mouseleave",()=>{
+        mouseOnWhole = false;
+        setTimeout(()=>{
+            if(!mouseOnToggle){
+                dropdownToggleEle.classList.remove('active');
+                leave();
+            }
+        },200)
+    }, false);
+
+    toBeActiveArray.forEach(e=>{
+        e.addEventListener("mouseenter",()=>{
+            e.classList.add('active');
+        },false);
+        
+        e.addEventListener("mouseleave",()=>{
+          e.classList.remove('active');
+      },false);
+    })
+    //NOTE: why e.addEventListener("focus" 无效？（触发了事件但未添加/移除类
+    //NOTE: console.log(e)打印了a元素的href信息
+   
+    const dropdownEleArray = Array.from(wholeControl.querySelectorAll('.dropdown-item:not(.current)'));
+
+    wholeControl.addEventListener("focusin", ()=>{
+      mouseOnWhole = true;
+    }, false);
+
+    dropdownEleArray.forEach(e=>{
+      e.addEventListener("focus",()=>{
+        mouseOnWhole = true;
+        e.classList.add('active');
+        //console.log(e+"focus");
+      },false);
+
+      e.addEventListener("blur",()=>{
+        e.classList.remove('active');
+        mouseOnWhole = false;
+        setTimeout(()=>{
+          if(!mouseOnToggle && !mouseOnWhole){
+              dropdownToggleEle.classList.remove('active');
+              leave();
+          }
+        },1)
+      },false);
+    })
+    
+    let displayStyle = window.matchMedia("(max-width: 1330px)").matches?"flex":"block";
+
+    const toggleInFunction = ()=>{
+      mouseOnToggle = true;
+      wholeControl.style.display = displayStyle;
+      currentCategory.classList.add('show');
+      dropdownEleArray.forEach((e,i)=>{
+          setTimeout(()=>{
+              e.classList.add('show');
+          },80 * i)
+      })
+      if(setStyleOfDiv===false){
+        if(displayStyle!=="flex"){
+          wholeControl.style.top = `${dropdownToggleEle.offsetTop + 12 - currentCategory.offsetTop}px`;
+          wholeControl.style.left = `${dropdownToggleEle.clientWidth - 2}px`;
+        } else {
+          wholeControl.style.top = `${dropdownToggleEle.offsetTop - currentCategory.offsetTop - 5}px`;
+          wholeControl.style.left = `${ wholeControl.clientWidth - currentCategory.offsetLeft}px`;
+          dropdownToggleEle.style.paddingRight = `${ wholeControl.clientWidth - currentCategory.offsetLeft - 54}px`;
+        }
+        setStyleOfDiv = true;
+      }
+    }
+    dropdownToggleEle.addEventListener("mouseenter", ()=>{
+      toggleInFunction();
+    }, false);
+
+    dropdownToggleEle.addEventListener("focus", ()=>{
+      toggleInFunction();
+    }, false);
+
+    const toggleOutFunction = (time)=>{
+      mouseOnToggle = false;
+      dropdownToggleEle.classList.add('active');
+      setTimeout(()=>{
+      if(!mouseOnWhole){
+          dropdownToggleEle.classList.remove('active');
+          leave();
+      }},time)
+    };
+    dropdownToggleEle.addEventListener("mouseleave",()=>{
+      toggleOutFunction(200);
+    }, false);
+
+    dropdownToggleEle.addEventListener("blur",()=>{
+      toggleOutFunction(1);
+    }, false);
+
+    function leave(){
+        wholeControl.style.display = "none";
+        Array.from(wholeControl.querySelectorAll('.dropdown-item')).forEach(e=>{
+            e.classList.remove('show');
+        })
+    }
+ }
+})()
--- pwa.js
+++ pwa.js
@@ -1,1 +1,14 @@
-"use strict";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/serviceWorker.js",{scope:"/"}).then(function(e){e.installing&&console.log("[ServiceWorker] installing")}).catch(function(e){console.log("Registration failed with "+e)})});
+// register service worker
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker.register('/serviceWorker.js', { scope: '/' }).then(function(reg) {
+          if(reg.installing) {
+            console.log('[ServiceWorker] installing');
+          }
+        }).catch(function(error) {
+          // registration failed
+          console.log('Registration failed with ' + error);
+        });
+    });
+  }
