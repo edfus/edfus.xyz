b46adfd2241649f323483ed68a5c1b888d8eaeb0
edfus <edfusedfus@gmail.com>
2020-07-20 19:08:49 +0800

    Site updated: 2020-07-20 19:08:34

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,9 +7,9 @@ const cacheResources = [
   "https://cdn.jsdelivr.net/gh/edfus/storage/images/root/ddby_logo.png.webp",
   "https://cdn.jsdelivr.net/gh/edfus/storage/images/root/footer-reimu.png.webp",
   "https://cdn.jsdelivr.net/gh/edfus/storage/images/root/font-display.png.webp",
-  "https://cdn.jsdelivr.net/gh/edfus/storage/images/mobile-SA/74309455_p4_crop.jpg.webp",
-  "https://cdn.jsdelivr.net/gh/edfus/storage/js/script.js",
-  "https://cdn.jsdelivr.net/gh/edfus/storage/glightbox/glightbox_modified.min.js"
+  "https://cdn.jsdelivr.net/gh/edfus/storage/images/mobile-SA/74309455_p4_crop.jpg.webp"
+  // "https://cdn.jsdelivr.net/gh/edfus/storage/js/script.js",
+  // "https://cdn.jsdelivr.net/gh/edfus/storage/glightbox/glightbox_modified.min.js"
 ] 
 //FIX: babel binding error code
 //NOTE: it seems that cacheResources is used only after first install.
