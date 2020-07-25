3b5f0803570a8f107485e7581013ed42b121e78d
edfus <edfusedfus@gmail.com>
2020-07-25 14:53:14 +0800

    Site updated: 2020-07-25 14:53:02

--- loadFonts.js
+++ loadFonts.js
@@ -1,17 +1,1 @@
-if("indexedDB" in window && "Worker" in window){
-  const fontLoadWorker = new Worker("/webWorker.js");
-
-	fontLoadWorker.onmessage = fontLoadedEvent => {
-    ((name = fontLoadedEvent.data.name, url = fontLoadedEvent.data.url) => {
-      if(!Array.from(document.fonts).some(e=>{return e.family === name})){
-        let newFont = new FontFace(name, `url("${url}")`, {
-          style: 'normal', weight: '400'
-        })
-        newFont.load().then(function() {
-          document.fonts.add(newFont);
-        });
-        console.log(name + " added");
-      }
-    })()
-  }
-}
+if("indexedDB"in window&&"Worker"in window){new Worker("/webWorker.js").onmessage=e=>{((n=e.data.name,o=e.data.url)=>{if(!Array.from(document.fonts).some(e=>e.family===n)){let e=new FontFace(n,`url("${o}")`,{style:"normal",weight:"400"});e.load().then((function(){document.fonts.add(e)})),console.log(n+" added")}})()}}
